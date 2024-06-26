0010952fc2f1a84d3f0f3ca3d53af656911a85fb
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-06-26 21:26:40 +0200

    update names

--- Raspberry-Pi-1b/lib/Effects.py
+++ Raspberry-Pi-1b/lib/Effects.py
@@ -2,14 +2,14 @@ from enum import Enum
 
 # These effects have been programmed on the Pico.
 class Effect(Enum):
-    STATIC = 1,
-    RAINBOW = 2,
-    RAINBOW_CYCLE = 3,
-    THEATER_CHASE = 4,
-    THEATER_CHASE_RAINBOW = 5,
-    COLOR_WIPE = 6,
-    SCANNER = 7,
-    FADE = 8,
-    TWINKLE = 9,
-    FLASH = 10,
+    STATIC = "STATIC",
+    RAINBOW = "RAINBOW",
+    RAINBOW_CYCLE = "RAINBOW_CYCLE",
+    THEATER_CHASE = "THEATER_CHASE",
+    THEATER_CHASE_RAINBOW = "THEATER_CHASE_RAINBOW",
+    COLOR_WIPE = "COLOR_WIPE",
+    SCANNER = "SCANNER",
+    FADE = "FADE",
+    TWINKLE = "TWINKLE",
+    FLASH = "FLASH",
     
