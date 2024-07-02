cdad1b0ab4ea1c4a4da183db61170ea096a2ad33
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-07-02 15:59:25 +0200

    change color

--- Raspberry-Pi-1b/eventLoop.py
+++ Raspberry-Pi-1b/eventLoop.py
@@ -1,7 +1,7 @@
 from lib.TimedEvent import TimedEvent
 from lib.Effects import Effect
 from lib.Groups import Groups
-from lib.Colors import blue, gold, green, aquamarine
+from lib.Colors import blue, gold, green, turquoise
 
 # typed array
 eventLoop: list[TimedEvent] = []
@@ -23,5 +23,5 @@ addEvent(0, 0, Effect.PULSATE, Groups.LEDKAST_2, blue)
 addPauzeEvent(0, 4000, Effect.STATIC, Groups.LEDKAST_1, green)
 
 addPauzeEvent(4000, 6000, Effect.STATIC, Groups.LEDKAST_1, gold)
-addPauzeEvent(6000, 10000, Effect.STATIC, Groups.LEDKAST_1, aquamarine)
+addPauzeEvent(6000, 10000, Effect.STATIC, Groups.LEDKAST_1, turquoise)
 
