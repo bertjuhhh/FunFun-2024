4459405137cd70072e905332eda48f997936d65e
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-07-04 16:24:58 +0200

    update eventloop

--- Raspberry-Pi-1b/eventLoop.py
+++ Raspberry-Pi-1b/eventLoop.py
@@ -17,10 +17,10 @@ def addPauzeEvent(start, effect: Effect, group, color = None):
     pauzeLoop.append(event)
     
 def clearAll(loop):
-    group1 = TimedEvent(start=0, effect=Effect.CLEAR, group=Groups.LEDKAST_1)
-    group2 = TimedEvent(start=1000, effect=Effect.CLEAR, group=Groups.LEDKAST_2)
-    group3 = TimedEvent(start=2000, effect=Effect.CLEAR, group=Groups.LEDKAST_3)
-    group4 = TimedEvent(start=3000, effect=Effect.CLEAR, group=Groups.LEDKAST_4)
+    group1 = TimedEvent(start=0, effect=Effect.STATIC, group=Groups.LEDKAST_1, color=clear)
+    group2 = TimedEvent(start=1000, effect=Effect.STATIC, group=Groups.LEDKAST_2, color=clear)
+    group3 = TimedEvent(start=2000, effect=Effect.STATIC, group=Groups.LEDKAST_3, color=clear)
+    group4 = TimedEvent(start=3000, effect=Effect.STATIC, group=Groups.LEDKAST_4, color=clear)
     
     loop.append(group1)
     loop.append(group2)
@@ -38,6 +38,6 @@ addPauzeEvent(5500, Effect.PULSATE, Groups.LEDKAST_2, green)
 addPauzeEvent(6000, Effect.PULSATE, Groups.LEDKAST_3, turquoise)
 addPauzeEvent(6500, Effect.PULSATE, Groups.LEDKAST_4, gold)
 
-addPauzeEvent(13000, Effect.CLEAR, Groups.LEDKAST_2)
+addPauzeEvent(13000, Effect.STATIC, Groups.LEDKAST_2, clear)
 addPauzeEvent(18000, Effect.PULSATE, Groups.LEDKAST_2, magenta)
 
