20a3e3193deb328770df3b9fad717f8289f19ef8
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-08-09 12:42:32 +0200

    update delay

--- Raspberry-Pi-1b/music.py
+++ Raspberry-Pi-1b/music.py
@@ -209,7 +209,7 @@ def main():
             resetAllEffects(active_song[1])
            
         # Check every 500ms
-        if millis() - last_time >= 100:
+        if millis() - last_time >= 500:
             last_time = millis()
             currentTime = millis()
             
