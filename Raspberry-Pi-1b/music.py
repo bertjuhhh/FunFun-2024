cb17102166c798159ad878bc95e0b9704c870ab9
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-08-09 13:50:18 +0200

    update delay

--- Raspberry-Pi-1b/music.py
+++ Raspberry-Pi-1b/music.py
@@ -226,6 +226,6 @@ def main():
                 if event.shouldStart(currentTime, startTime):
                     sendCommand(event, currentRelativeTime)
                     writeLCD_line_2(f"{groupNumber} > START {event.effect.value}")
-        time.sleep(0.01)
+        time.sleep(0.001)
                     
 main()
