32f88f8d036ed33e70978e48b02baef27e756b87
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-08-09 15:49:04 +0200

    pulsate

--- Raspberry-Pi-Pico/effects/pulsate_leds.py
+++ Raspberry-Pi-Pico/effects/pulsate_leds.py
@@ -4,17 +4,27 @@ async def pulsateLEDs(ledkast, color):
     brightness = 0
     direction = 1
     
-    color = (0, 0, 255)
+    color = (255, 0, 255)  # Blue color, adjust as needed
     
     while True:
+        # Update the color of each LED with the current brightness
         for i in range(ledkast.strips.n):
-            ledkast.strips[i] = (color[0] * brightness // 255, color[1] * brightness // 255, color[2] * brightness // 255)
+            ledkast.strips[i] = (color[0] * brightness // 255, 
+                                 color[1] * brightness // 255, 
+                                 color[2] * brightness // 255)
             
         ledkast.strips.write()
         
+        # Update brightness
         brightness += direction
         
-        if brightness <= 0 or brightness >= 255:
-            direction *= -1
+        # Reverse direction at bounds
+        if brightness <= 0:
+            brightness = 0
+            direction = 1
+        elif brightness >= 255:
+            brightness = 255
+            direction = -1
             
+        # Delay to control pulsation speed
         await asyncio.sleep(0.01)
