fde9fb6e0d05d8215d2075c2f9ce8e51ebafdc9b
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-08-15 22:34:31 +0200

    updated eventloop

--- Raspberry-Pi-1b/partyLoop.py
+++ Raspberry-Pi-1b/partyLoop.py
@@ -1,31 +1,25 @@
-from eventLoop import addEventToLoop
+from eventLoop import addToGroup
+from lib.Groups import outside_group
 from lib.Effects import Effect
 from lib.Colors import clear, lime, yellow, gold, blue, pink, yellow, red
-from lib.Groups import Groups
 
 partyLoop = []
 
 startMarker = 1240
 
-addEventToLoop(partyLoop, startMarker, Effect.PULSATE, Groups.ALL, lime)
+addToGroup(partyLoop, startMarker, Effect.PULSATE, outside_group, lime)
 
 buildupMarker = 21540
 
-addEventToLoop(partyLoop, buildupMarker, Effect.PULSATE, blue)
+addToGroup(partyLoop, buildupMarker, Effect.PULSATE, blue)
 
 climaxMarker = 31570
 
-addEventToLoop(partyLoop, climaxMarker, Effect.BPMFLASH, Groups.LEDKAST_1, blue)
-addEventToLoop(partyLoop, climaxMarker, Effect.BPMFLASH, Groups.LEDKAST_2, pink)
-addEventToLoop(partyLoop, climaxMarker, Effect.BPMFLASH, Groups.LEDKAST_3, yellow)
-addEventToLoop(partyLoop, climaxMarker, Effect.BPMFLASH, Groups.LEDKAST_4, lime)
+addToGroup(partyLoop, climaxMarker, Effect.BPMFLASH, outside_group, pink)
 
 beforeBeatMarker = 55750
 beatMarker = 56660
 
-addEventToLoop(partyLoop, beforeBeatMarker, Effect.FLASH, Groups.ALL, red)
+addToGroup(partyLoop, beforeBeatMarker, Effect.FLASH, outside_group, red)
 
-addEventToLoop(partyLoop, beatMarker, Effect.BPMFLASH, Groups.LEDKAST_1, red)
-addEventToLoop(partyLoop, beatMarker, Effect.CHASE, Groups.LEDKAST_2, red)
-addEventToLoop(partyLoop, beatMarker, Effect.PULSATE, Groups.LEDKAST_3, red)
-addEventToLoop(partyLoop, beatMarker, Effect.CHASE, Groups.LEDKAST_3, red)
+addToGroup(partyLoop, beatMarker, Effect.PULSATE, outside_group, red)
--- Raspberry-Pi-Pico/effects/chase_lights.py
+++ Raspberry-Pi-Pico/effects/chase_lights.py
@@ -1,31 +1,34 @@
 import asyncio
 
-async def chase(ledkast, color, line_length=5, speed=0.05):
-    position = 0
-    direction = 1
+async def chaseLEDs(ledkast, color, start_position, delay=0.05, fade_steps=10):
+    n_leds = ledkast.strips.n
+    position = start_position
 
     while True:
-        # Clear the strip
-        for i in range(ledkast.strips.n):
-            ledkast.strips[i] = (0, 0, 0)
-
-        # Draw the moving line
-        for i in range(line_length):
-            if 0 <= position + i < ledkast.strips.n:
-                ledkast.strips[position + i] = color
+        for i in range(n_leds):
+            ledkast.strips[i] = (0, 0, 0)  # Turn off all LEDs
+            
+        for j in range(n_leds):
+            offset_position = (position + j) % n_leds
+            fade_factor = max(0, (fade_steps - abs(j)) / fade_steps)
+            ledkast.strips[offset_position] = (
+                int(color[0] * fade_factor),
+                int(color[1] * fade_factor),
+                int(color[2] * fade_factor),
+            )
         
         ledkast.strips.write()
+        await asyncio.sleep(delay)
+        
+        # Move the chase position
+        position = (position + 1) % n_leds
 
-        # Update position
-        position += direction
+# Function to start multiple chases
+async def chase(ledkast, color, num_chases=3, delay_between_chases=0.2):
+    tasks = []
+    for i in range(num_chases):
+        start_position = (i * ledkast.strips.n // num_chases) % ledkast.strips.n
+        tasks.append(asyncio.create_task(chaseLEDs(ledkast, color, start_position)))
+        await asyncio.sleep(delay_between_chases)
 
-        # Reverse direction at bounds
-        if position <= 0:
-            position = 0
-            direction = 1
-        elif position + line_length >= ledkast.strips.n:
-            position = ledkast.strips.n - line_length
-            direction = -1
-
-        # Delay to control movement speed
-        await asyncio.sleep(speed)
+    await asyncio.gather(*tasks)
