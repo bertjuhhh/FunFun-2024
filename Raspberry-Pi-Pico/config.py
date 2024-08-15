530ff4cbb45170de7c16d2f72acbe63e0812157d
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2024-08-15 15:14:17 +0200

    updated config

--- Raspberry-Pi-Pico/config.py
+++ Raspberry-Pi-Pico/config.py
@@ -10,31 +10,31 @@ UART_TX_PIN = 4
 UART_RX_PIN = 5
 UART_BAUDRATE = 9600
 
-LEDKAST_1 = Ledkast(pin=0, ledCount=300, name="LEDKAST_1") 
-LEDKAST_1b = Ledkast(pin=1, ledCount=300, name="LEDKAST_1b") 
-LEDKAST_2 = Ledkast(pin=2, ledCount=300, name="LEDKAST_2") 
-LEDKAST_2b = Ledkast(pin=3, ledCount=300, name="LEDKAST_2b") 
-LEDKAST_3 = Ledkast(pin=12, ledCount=300, name="LEDKAST_3") 
-LEDKAST_3b = Ledkast(pin=13, ledCount=300, name="LEDKAST_3b") 
-LEDKAST_4 = Ledkast(pin=14, ledCount=300, name="LEDKAST_4") 
-LEDKAST_4b = Ledkast(pin=15, ledCount=300, name="LEDKAST_4b")
+# LEDKAST_1 = Ledkast(pin=0, ledCount=150, name="None") 
+# LEDKAST_1b = Ledkast(pin=1, ledCount=150, name="None") 
+# LEDKAST_2 = Ledkast(pin=2, ledCount=150, name="LEDKAST_2") 
+# LEDKAST_2b = Ledkast(pin=3, ledCount=150, name="None") 
+LEDKAST_3 = Ledkast(pin=12, ledCount=150, name="LEDKAST_3") 
+LEDKAST_3b = Ledkast(pin=13, ledCount=150, name="LEDKAST_3b") 
+LEDKAST_4 = Ledkast(pin=14, ledCount=150, name="LEDKAST_4") 
+LEDKAST_4b = Ledkast(pin=15, ledCount=150, name="LEDKAST_4b")
 
 # Indicators that are present on the hoofdkast
 EXTERNAL_INDICATORS = Ledkast(pin=16, ledCount=10 , name="EXTERNAL_INDICATORS")# Ledkast(board.GP6, 10) ool checken
 
 def getLedkast(group: str):
-    if group == "LEDKAST_1":
-        return LEDKAST_1
+    if group == "None":
+        return None
     elif group == "LEDKAST_2":
-        return LEDKAST_2
+        return None
     elif group == "LEDKAST_3":
         return LEDKAST_3
     elif group == "LEDKAST_4":
         return LEDKAST_4
     elif group == "LEDKAST_1b":
-        return LEDKAST_1b
+        return None
     elif group == "LEDKAST_2b":
-        return LEDKAST_2b
+        return None
     elif group == "LEDKAST_3b":
         return LEDKAST_3b
     elif group == "LEDKAST_4b":
@@ -45,7 +45,7 @@ def getLedkast(group: str):
         return None
     
 def getAllLedkasts() -> list[Ledkast]:
-    return [LEDKAST_1, LEDKAST_2, LEDKAST_3, LEDKAST_4, LEDKAST_1b, LEDKAST_2b, LEDKAST_3b, LEDKAST_4b, EXTERNAL_INDICATORS]
+    return [None, None, LEDKAST_3, LEDKAST_4, None, None, LEDKAST_3b, LEDKAST_4b, EXTERNAL_INDICATORS]
 
 def getAllLedkastsExceptExternal() -> list[Ledkast]:
-    return [LEDKAST_1, LEDKAST_2, LEDKAST_3, LEDKAST_4]
+    return [None, None, LEDKAST_3, LEDKAST_4]
